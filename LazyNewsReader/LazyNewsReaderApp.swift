1b1015b1602df1c3f915bc732ffba1c53b172c8b
vikas.soni <vikas.soni@cardinalhealth.com>
2025-05-04 20:49:20 +0530

    Initial Commit

---
+++ LazyNewsReader/LazyNewsReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  LazyNewsReaderApp.swift
+//  LazyNewsReader
+//
+//  Created by Vikas Soni on 04/05/25.
+//
+
+import SwiftUI
+
+@main
+struct LazyNewsReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
